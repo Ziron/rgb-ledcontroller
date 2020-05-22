300352e23abe99a913e9bd2489c151374733544b
Sebastian Gustavsson <sebgu182@student.liu.se>
2020-05-22 22:07:17 +0200

    Initial commit

---
+++ ledshelfcontroller.ino
@@ -0,0 +1,32 @@
+
+
+// ESP_TX - A0 - PC0
+// ESP_RX - A1 - PC1
+
+// G1 - A2 -- PC2
+// R1 - A3 -- PC3
+// B1 - A4 -- PC4
+// G2 - A5 -- PC5
+// R2 - D12 - PB4
+// B2 - D11 - PB3
+// G3 - D10 - PB2
+// R3 - D9 -- PB1
+// B3 - D8 -- PB0
+// G4 - D7 -- PD7
+// R4 - D6 -- PD6
+// B4 - D5 -- PD5
+// G5 - D4 -- PD4
+// R5 - D3 -- PD3
+// B5 - D2 -- PD2
+
+// Serial RX/TX is PD0/1
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
