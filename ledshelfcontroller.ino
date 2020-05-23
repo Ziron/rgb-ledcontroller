0a97f73ca5e462aa3eab13e89c4dbd0786bd9411
Sebastian Gustavsson <sebgu182@student.liu.se>
2020-05-23 20:31:52 +0200

    Finished?

--- ledshelfcontroller.ino
+++ ledshelfcontroller.ino
@@ -23,6 +23,53 @@
 
 // Serial RX/TX is PD0/1
 
+//--------------------
+#define G1_ARR C_STATE
+#define G1_BIT (1 << 2)
+
+#define R1_ARR C_STATE
+#define R1_BIT (1 << 3)
+
+#define B1_ARR C_STATE
+#define B1_BIT (1 << 4)
+//--------------------
+#define G2_ARR C_STATE
+#define G2_BIT (1 << 5)
+
+#define R2_ARR B_STATE
+#define R2_BIT (1 << 4)
+
+#define B2_ARR B_STATE
+#define B2_BIT (1 << 3)
+//--------------------
+#define G3_ARR B_STATE
+#define G3_BIT (1 << 2)
+
+#define R3_ARR B_STATE
+#define R3_BIT (1 << 1)
+
+#define B3_ARR B_STATE
+#define B3_BIT (1 << 0)
+//--------------------
+#define G4_ARR D_STATE
+#define G4_BIT (1 << 7)
+
+#define R4_ARR D_STATE
+#define R4_BIT (1 << 6)
+
+#define B4_ARR D_STATE
+#define B4_BIT (1 << 5)
+//--------------------
+#define G5_ARR D_STATE
+#define G5_BIT (1 << 4)
+
+#define R5_ARR D_STATE
+#define R5_BIT (1 << 3)
+
+#define B5_ARR D_STATE
+#define B5_BIT (1 << 2)
+//--------------------
+
 SoftwareSerial espSerial(A0, A1); // RX, TX
 
 const int PWM_CYCLE = 256;
@@ -37,6 +84,30 @@ const char B_MASK = 0b00011111;
 const char C_MASK = 0b00111100;
 const char D_MASK = 0b11111100;
 
+int R1_val = 0;
+int G1_val = 0;
+int B1_val = 0;
+
+int R2_val = 0;
+int G2_val = 0;
+int B2_val = 0;
+
+int R3_val = 0;
+int G3_val = 0;
+int B3_val = 0;
+
+int R4_val = 0;
+int G4_val = 0;
+int B4_val = 0;
+
+int R5_val = 0;
+int G5_val = 0;
+int B5_val = 0;
+
+int cur_val = 0;
+int val_count = 0;
+int char_count = 0;
+
 ISR(TIMER2_COMPA_vect){
    //interrupt commands here
 
@@ -47,6 +118,21 @@ ISR(TIMER2_COMPA_vect){
    if (++pwm_i >= PWM_CYCLE) {
       pwm_i = 0;
    }
+}
+
+void setVal(volatile char *arr, char valBit, int &curVal, int newVal) {
+  if (newVal == curVal) {
+    return;
+  } else if (newVal < curVal) {
+    for (int i = curVal - 1; i >= newVal; i--) {
+      arr[i] &= ~valBit;
+    }
+  }else {
+    for (int i = curVal; i < newVal; i++) {
+      arr[i] |= valBit;
+    }
+  }
+  curVal = newVal;
 }
 
 int hexToInt(char hexChar) {
@@ -91,8 +177,94 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  if (espSerial.available()) {
+  if (espSerial.available())
+  {
     char c = espSerial.read();
+
+    if (c == ';') {
+      cur_val = 0;
+      val_count = 0;
+      char_count = 0;
+    } else {
+      cur_val = (cur_val << 8) |  hexToInt(c);
+
+      if (++char_count >= 2) {
+
+        switch(val_count++) {
+          case 0: { // R1
+            setVal(R1_ARR, R1_BIT, R1_val, cur_val);
+            break;
+          }
+          case 1: { // G1
+            setVal(G1_ARR, G1_BIT, G1_val, cur_val);
+            break;
+          }
+          case 2: { // B1
+            setVal(B1_ARR, B1_BIT, B1_val, cur_val);
+            break;
+          }
+
+          case 3: { // R2
+            setVal(R2_ARR, R2_BIT, R2_val, cur_val);
+            break;
+          }
+          case 4: { // G2
+            setVal(G2_ARR, G2_BIT, G2_val, cur_val);
+            break;
+          }
+          case 5: { // B2
+            setVal(B2_ARR, B2_BIT, B2_val, cur_val);
+            break;
+          }
+
+          case 6: { // R3
+            setVal(R3_ARR, R3_BIT, R3_val, cur_val);
+            break;
+          }
+          case 7: { // G3
+            setVal(G3_ARR, G3_BIT, G3_val, cur_val);
+            break;
+          }
+          case 8: { // B3
+            setVal(B3_ARR, B3_BIT, B3_val, cur_val);
+            break;
+          }
+
+          case 9: { // R4
+            setVal(R4_ARR, R4_BIT, R4_val, cur_val);
+            break;
+          }
+          case 10: { // G4
+            setVal(G4_ARR, G4_BIT, G4_val, cur_val);
+            break;
+          }
+          case 11: { // B4
+            setVal(B4_ARR, B4_BIT, B4_val, cur_val);
+            break;
+          }
+
+          case 12: { // R5
+            setVal(R5_ARR, R5_BIT, R5_val, cur_val);
+            break;
+          }
+          case 13: { // G5
+            setVal(G5_ARR, G5_BIT, G5_val, cur_val);
+            break;
+          }
+          case 14: { // B5
+            setVal(B5_ARR, B5_BIT, B5_val, cur_val);
+            break;
+          }
+
+          default: {
+            break;
+          }
+        }
+        
+        cur_val = 0;
+      }
+    }
+    
   }
 
 }
